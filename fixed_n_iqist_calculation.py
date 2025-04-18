755fcdc5974580add493ce983577fd5dc9e4d968
ivngrmk <goremykin.ia@phystech.su>
2025-04-18 09:33:19 +0300

    Added creation of log.txt file for fixed_n calculations.

--- fixed_n_iqist_calculation.py
+++ fixed_n_iqist_calculation.py
@@ -14,3 +14,8 @@ class FixedNiQISTCalculation(iQISTCalculation):
     @property
     def label(self) -> str:
         return super().label + f"occ{self.parameters["occ"]:.4f}qx{self.parameters["qx"]:.4f}qy{self.parameters["qy"]:.4f}"
+
+    def setup(self, calculation_dir: str):
+        with open(os.path.join(calculation_dir,"log.txt"),'w') as log_f:
+            log_f.write("\n")
+        return super().setup(calculation_dir)
