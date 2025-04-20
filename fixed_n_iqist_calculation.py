668498b275bc30781fe4f6b6d33f1e1a362bb2fd
ivngrmk <goremykin.ia@phystech.su>
2025-04-20 22:05:00 +0300

    Added taking of chemical potential from solver.conv.dat for calculations with fixed n.

--- fixed_n_iqist_calculation.py
+++ fixed_n_iqist_calculation.py
@@ -1,3 +1,5 @@
+from typing import Self
+
 from .iqist_calculation import iQISTCalculation
 import numpy as np
 import os
@@ -19,3 +21,19 @@ class FixedNiQISTCalculation(iQISTCalculation):
         with open(os.path.join(calculation_dir,"log.txt"),'w') as log_f:
             log_f.write("\n")
         return super().setup(calculation_dir)
+    
+    @classmethod
+    def from_calculation(cls, calc_dirn: str, *kargs, **kwargs) -> Self:
+        calc = super().from_calculation(calc_dirn, *kargs, **kwargs)
+
+        conv_fn = os.path.join(calc_dirn, "solver.conv.dat")
+        if os.path.isfile(conv_fn):
+            with open(conv_fn, 'r') as conv_f:
+                header = conv_f.readline()
+                column_labels = header.strip().split(sep=',')
+                if column_labels[-1].strip() == "mune":
+                    conv_data = np.loadtxt(conv_fn, skiprows=1)
+                    mu = conv_data[-1,-1]
+                    calc.parameters["mu"] = mu
+
+        return calc
